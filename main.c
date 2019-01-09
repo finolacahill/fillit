d9e6e281688c4d1397a5fb4c8da2384fec7a3f8c
Finola CAHILL <fcahill@e2r7p17.42.fr>
2019-01-09 14:05:19 +0100

    commented

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: fcahill <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 23:28:43 by fcahill           #+#    #+#             */
-/*   Updated: 2019/01/09 13:33:03 by fcahill          ###   ########.fr       */
+/*   Updated: 2019/01/09 14:05:07 by fcahill          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft/libft.h"
-
+//This is very much just a test main. It's a giant mess but it's a workign mess. To be cleaned up.
 int		main(void)
 {
 	int		fd;
@@ -28,6 +28,7 @@ int		main(void)
 	fd = open("valide_26", O_RDONLY);
 	while ((n = read(fd, str, 5000)) > 0)
 			str[n] = '\0';
+	//from here
 	count = ft_count_tmn(str);
 	if (ft_1validate(str) != 1)
 	{
@@ -46,6 +47,9 @@ int		main(void)
 	size = ft_minsize(count);
 	map = ft_create_map(size, map);
 	i = 0;
+	//to here, we're just creating our map and saving our tetris pieces. 
+
+//this is the solving part, without backtracking. 
 	while (i < count)
 	{
 		p = 0;
@@ -53,22 +57,22 @@ int		main(void)
 
 		while (i < count && (n = ft_check_map(map, table[i], p, q)) == 0)
 		{ 
-			if (q != size)
+			if (q != size) //if it's not the end of the line
 				q++;
-			if (q == size && p != size)
+			if (q == size && p != size) //if it's the end of the line but not the end of the map
 			{
 				p++;
 				q = 0;
 			}
-			if (p == size)
+			if (p == size) //it's the end of the map
 				break;
 		}
-		if (n > 0)
+		if (n > 0) // we could place all the pieces on the map
 		{
 			map = ft_place(map, table[i], p, q);
 			i++;
 		}
-		if (n == 0)
+		if (n == 0) // we couldn't place all the pieces, so we increase map size
 		{
 		 	ft_strdel(map);
 			size++;
@@ -76,10 +80,11 @@ int		main(void)
 			map = ft_create_map(size, map);
 		}
 	}
-	n = 0;
+	n = 0; //this is to print the map
 	while (n < size)
 	{
-	printf("%s\n", map[n]);
+	ft_putstr(map[n]);
+	ft_putchar('\n');
 	++n;
 	}
 return (0);
